e97e3e0493ed19a67dd2dcca4c17058ee2792135
Jared Veltsos <veltsos.jared@gmail.com>
2022-08-08 16:02:47 -1000

    webpack setup, base react structure

---
+++ client/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+      </div>
+    );
+    return element;
+  }
+
+  export default App
