f519c9ba8ed943a14ca14fece71e70c8e517d48a
Jared Veltsos <veltsos.jared@gmail.com>
2022-08-09 21:57:26 -1000

    formatted files

--- client/components/AppVersion.jsx
+++ client/components/AppVersion.jsx
@@ -4,7 +4,7 @@ import { Typography } from '@mui/material';
 function AppVersion({ appVersion }) {
   return (
     <div style={{ border: '1px solid black', width: 'max-content' }}>
-     <Typography>AppVersion</Typography>
+      <Typography>AppVersion</Typography>
       {appVersion}
     </div>
   );
--- client/components/BugTitle.jsx
+++ client/components/BugTitle.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-
 import { Typography } from '@mui/material';
 
 function BugTitle({ title }) {
   return (
-    <div style={{ border: '1px solid black', textAlign: 'center'}}>
-     <Typography>BugTitle</Typography>
+    <div style={{ border: '1px solid black', textAlign: 'center' }}>
+      <Typography>BugTitle</Typography>
       {title}
     </div>
   );
