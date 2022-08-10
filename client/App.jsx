75c04c23b0fb727f8a9ff298265e0800c6d8c553
MH <mhgmel00@gmail.com>
2022-08-10 13:31:02 -0700

    login context

--- client/App.jsx
+++ client/App.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import UserContext from './UserContext';
 import Dashboard from './pages/DashboardContainer';
 import LogIn_SignUp from './pages/LogIn_SignUp';
 import SignUp from './pages/SignUp';
 import AddBug from './pages/AddBug';
 
 function App() {
+  const [user, setUser] = useState('user login object');
+  const providerValue = useMemo(() => ({ user, setUser }), [user, setUser])
+
   const element = (
     <div>
+      
       <BrowserRouter>
+      <UserContext.Provider value={providerValue}>
         <Routes>
           <Route index element={<Dashboard />} />
           <Route path='addBug' element={<AddBug />} />
           <Route path='login' element={<LogIn_SignUp />} />
           <Route path='signup' element={<SignUp />} />
         </Routes>
+      </UserContext.Provider>
       </BrowserRouter>
+      
     </div>
   );
   return element;
--- client/pages/LogIn_SignUp.jsx
+++ client/pages/LogIn_SignUp.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FormControl, TextField, Button } from '@mui/material';
 import NavBar from '../components/NavBar';
+import UserContext from '../UserContext';
 
 const initialCredential = {
   username: "",
@@ -9,15 +10,36 @@ const initialCredential = {
 };
 
 const LogIn_SignUp = () => {
+  const { user, setUser } = useContext(UserContext);
   const [credential, setCredential] = useState(initialCredential);
+  const [errText, setErrText] = useState('')
+
   const handleInputChange = (e) => {
     const { id, value } = e.target;
     setCredential({ ...credential, [id]:value })
+    setErrText('')
   }
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log(credential) // post request to login endpoint
+    if(!credential.username || !credential.password) {
+      setErrText('Please enter valid input')
+    } else {
+      console.log(credential) 
+      // post request to login endpoint
+      fetch('/login', {
+        method:'POST', 
+        headers:{ 'Content-Type': 'application/json' },
+        body: JSON.stringify(credential)
+      }).then(res => {
+        // redirection
+        if(res.status === 200) {
+          // ======= SAVE USER DATA ==========
+          setUser('some user')  // <<<<<<<<<<<< HERE
+          window.location.href = '/'
+        }
+      })
+    }
   }
 
   return (
@@ -25,10 +47,13 @@ const LogIn_SignUp = () => {
       <NavBar />
 
       <div style={{textAlign:'center'}}>
+        {user}
         <FormControl variant="standard">
-          <TextField id="username" value={credential.username} onChange={handleInputChange} label="User Name" variant="standard" />
-          <TextField id="password" type="password" value={credential.password} onChange={handleInputChange} label="Password" variant="standard" />
+          <TextField required id="username" value={credential.username} onChange={handleInputChange} label="User Name" variant="standard" />
+          <TextField required id="password" type="password" value={credential.password} onChange={handleInputChange} label="Password" variant="standard" />
           <Button type="submit" onClick={handleFormSubmit}>Sign In</Button>
+          <div style={{color:'red'}}>{errText}</div>
+          <br />
           <Link to='/signup'> Sign up for an account: </Link>
         </FormControl>
       </div>
