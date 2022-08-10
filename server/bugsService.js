63c3f2713f75b48c2d523eb0bea3236f46ac1d45
Brandon Brighi <brandon.erosenvironmental@gmail.com>
2022-08-10 15:33:53 -0700

    engineer notes and bugs combinded data complete

--- server/bugsService.js
+++ server/bugsService.js
@@ -47,7 +47,24 @@ const BugsService = {
     );
   },
 
-  // // inner join eNotes
+  getBugENotes(db, id) {
+    return db.raw(
+      `SELECT
+        "idENote" AS ID,
+        "noteBody" AS "Notes",
+        "noteTitle" AS "Note Title",
+        "submitter"."firstName" AS "Submitted By",
+        "bugnote"."title" AS "Bug Title",
+        "bugnote"."idBug" AS "Bug ID"
+        FROM "EngineeringNote"
+        INNER JOIN "Bug" bugnote ON "EngineeringNote"."idBug" = "bugnote"."idBug"
+        INNER JOIN "User" submitter ON "EngineeringNote"."idUser" = "submitter"."idUser"
+        WHERE "EngineeringNote"."idBug" = ${id}
+      `
+    );
+  },
+
+  // inner join eNotes
   // getAllEngineeringNotes(db) {
   //   return db.raw(`
   //   SELECT
